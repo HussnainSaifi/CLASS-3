6fc51e6c01b40719c5c9fe927806b7f58ce397eb
HussnainSaifi <saifisheikh663@gmail.com>
2026-03-06 21:56:04 -0800

    list practice
    on going

--- Python list practice.py
+++ Python list practice.py
@@ -1,3 +1,8 @@
 print("Wellcome to my lsit practice")
 customer_list = ["Hussnain", 5523, 35.7, True]
 print(customer_list)
+print(type(customer_list))
+customer_list.append("AbuBakar")
+print(customer_list)
+customer_list.insert(1, "Lahore")
+print(customer_list)
