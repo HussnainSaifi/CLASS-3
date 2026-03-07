6cbe8f03efd5a29f20cf79507eb4f910d56ef7b4
HussnainSaifi <saifisheikh663@gmail.com>
2026-03-06 22:31:00 -0800

    set practice
    done

---
+++ Set practice.py
@@ -0,0 +1,13 @@
+print("Welcome to my set practce")
+Customer_set = {"Hussnain", 5567, 56.8, False}
+print(Customer_set)
+
+Customer_set.add("Saifi")
+print(Customer_set)
+
+Customer_set.discard(5567)
+print(Customer_set)
+
+for i in Customer_set:
+    print(i)
+
