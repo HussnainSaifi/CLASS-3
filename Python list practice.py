0093ff20b51bf32768ab83b54432721dec4040b2
HussnainSaifi <saifisheikh663@gmail.com>
2026-03-06 22:12:10 -0800

    list practice
    done

--- Python list practice.py
+++ Python list practice.py
@@ -9,3 +9,17 @@ print(customer_list)
 
 customer_list.insert(1, "Lahore")
 print(customer_list)
+
+print(customer_list[1])
+print(customer_list[2])
+print(type(customer_list[2]))
+
+customer_list.remove(5523)
+print(customer_list)
+
+customer_list.pop(1)
+print(customer_list)
+
+
+customer_list[0] = "Saifi"
+print(customer_list)
