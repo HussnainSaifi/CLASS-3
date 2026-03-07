f255bd280a7ba8a1e09b48363c4a22b37ae85f64
HussnainSaifi <saifisheikh663@gmail.com>
2026-03-06 22:24:48 -0800

    tuple practice
    done

---
+++ My tuple practice.py
@@ -0,0 +1,7 @@
+print("Welcome to my tuple practice")
+customer_tuple = ("Saifi", 23, 25.4, False)
+print(customer_tuple)
+print(customer_tuple[-3])
+print(customer_tuple[1])
+for i in customer_tuple:
+    print(i)
