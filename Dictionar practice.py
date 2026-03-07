1c5a8bef5518a797e78eca9288adccb2d26f3716
HussnainSaifi <saifisheikh663@gmail.com>
2026-03-06 22:49:46 -0800

    Dictionary Practice
    done

--- Dictionar practice.py
+++ Dictionar practice.py
@@ -1,7 +1,9 @@
 print("Wlcome to my dictionary practice")
 customer_dic = {"Name":"Husnnian", "P-code": 334, "price": 55.6}
 print(customer_dic)
+
 customer_dic["Name"] = "Saifi"
 print(customer_dic)
+
 customer_dic["City"] = "Lahore"
 print(customer_dic)
